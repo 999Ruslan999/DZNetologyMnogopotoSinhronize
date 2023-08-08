b620a4d762d828db34df78c937e150e1d1ba6729
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-08-08 20:06:19 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+String letters = "RLRFR";
+int length = 100;
+int numThread = 5;
+
+        for (int i = 0; i < numThread; i++) {
+            Thread thread = new Thread(new Way(letters, length));
+            thread.start();
+
+        }
+
+
     }
 }
--- src/Way.java
+++ src/Way.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Way {
+import java.util.Random;
+
+public class Way implements Runnable {
+
+    private String letters;
+    private int length;
+
+    public Way(String letters, int length) {
+        this.letters = letters;
+        this.length = length;
+    }
+
+
+    @Override
+    public void run() {
+        String text = generateRoute(letters, length);
+        int count = countRightTurns(text);
+        System.out.println("Thread " + Thread.currentThread().getId() + "  --> " + count);
+
+    }
+
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+
+    public int countRightTurns(String text) {
+     int count = 0;
+        for (int i = 0; i < length; i++) {
+            if(text.charAt(i) == 'R') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
